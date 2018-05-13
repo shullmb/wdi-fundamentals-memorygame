1adf5470d2dd3645d944e1b265e7a462feb0e2b6
shullmb <dev.shullmb@gmail.com>
2018-05-13 15:14:37 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -3,7 +3,28 @@
 console.log('Up and running!');
 
 //Set up arrays for Memory Game
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -19,9 +40,11 @@ function checkForMatch() {
 
 function flipCard(cardId){
 
-    console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
     
-    cardsInPlay.push(cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank);
 
     checkForMatch();
 }
