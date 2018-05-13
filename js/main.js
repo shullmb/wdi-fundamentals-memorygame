31c4262ee3565c650a0130b79868f0be14373d89
shullmb <dev.shullmb@gmail.com>
2018-05-13 11:08:03 -0700

    created game logic

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* MEMORY GAME */
+
+console.log('Up and running!');
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
