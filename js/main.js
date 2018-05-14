750be545a821641be163485a0f66d18593379899
shullmb <dev.shullmb@gmail.com>
2018-05-13 18:46:05 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -28,26 +28,48 @@ var cards = [
 var cardsInPlay = [];
 
 function checkForMatch() {
-    if (cardsInPlay.length === 2) {
-    
-        if (cardsInPlay[0] === cardsInPlay[1]) {
-            alert('You found a match!');
+	
+	if (cardsInPlay.length === 2) {
+		
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert('You found a match!');
         } else {
-            alert('Sorry, try again.');
+			alert('Sorry, try again.');
         }
-    }
+	}
+	
 }
 
-function flipCard(cardId){
-
+function flipCard(){
+	
+	var cardId = this.getAttribute('data-id');
+	
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
     
     cardsInPlay.push(cards[cardId].rank);
-
-    checkForMatch();
+	
+	this.setAttribute('src', cards[cardId].cardImage);
+	
+	checkForMatch();
+	
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i );
+
+		cardElement.addEventListener('click', flipCard);
+
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+}
+
+createBoard();
+
+
